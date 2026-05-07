2fd35ed6e16ec9c32dcdaa7f0e81516bab5875bd
Max Paulus <m.paulus@uva.nl>
2026-05-07 16:00:17 +0200

    added websocket proxy

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 nginx_conf="/etc/nginx/conf.d/ssl_main.conf"
 
 # Let Nginx serve the docker container running on port 8080 instead of a static web page
-sed -i 's|root /var/www/html;|location \/ {\n    \tproxy_pass http:\/\/localhost:8080;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    }|' "$nginx_conf"
+sed -i 's|root /var/www/html;|location \/ {\n    \tproxy_pass http:\/\/localhost:8080;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    \tproxy_set_header Upgrade $http_upgrade;\n    \tproxy_set_header Connection "upgrade";\n    }|' "$nginx_conf"
 sed -i 's|index index.html index.htm;||' "$nginx_conf"
 
 systemctl restart nginx.service
